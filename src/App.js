6fcc091a1b6d22108c19e789e43c0ff9950c6956
Sanam Chaudhary <chysanan@gmail.com>
2024-12-31 08:40:12 +0545

    used hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './components/ThemeContainer'; // Theme context
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <ThemeProvider>
       <div className="ThemeContainer">
-        <Router>
+        <Router >
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
